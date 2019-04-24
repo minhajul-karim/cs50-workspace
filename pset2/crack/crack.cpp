73f5fef72440961762db82e718cc60099eb897c8
Minhajul Karim <rizon2637@gmail.com>
2019-04-24 16:18:30 +0600

    Add crack.cpp inside pset2/crack/

---
+++ pset2/crack/crack.cpp
@@ -0,0 +1,68 @@
+/**** The basic idea is adopted from
+https://www.geeksforgeeks.org/print-all-combinations-of-given-length/ ****/
+
+#include <iostream>
+#include <string>
+#include <crypt.h>
+#include <cstring>
+
+using namespace std;
+
+void crack(char alpha[], string prefix, char arg[], char salt[], int n, int k);
+
+int main(int argc, char** argv)
+{
+    // Check for appropriate user input
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: ./crack <hashed_key>\n");
+        return 0;
+    }
+
+    // Separate the salt form argv[1]
+    char salt[3];
+    strncpy(salt, argv[1], 2);
+    salt[2] = '\0';
+
+    // String of alphabets
+    char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    // Max length of a password
+    int max_password_len = 5;
+
+    // Check for password of length 1 to 5(max)
+    for (int i = 1; i <= max_password_len; i++)
+    {
+        crack(letters, "", argv[1], salt, elements, i);
+    }
+
+    printf("not found\n");
+
+    return 0;
+}
+
+void crack(char alpha[], string prefix, char arg[], char salt[], int n, int k)
+{
+    if (k == 0)
+    {
+        // Convert string into const char*
+        const char* c = prefix.c_str();
+
+        // Check if the encryption of current prefix matches user input i.e. argv[1]
+        if (strcmp(arg, crypt(c, salt)) == 0)
+        {
+            // Print the current prefix and exit
+            cout << prefix << endl;
+            exit(0);
+        }
+
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        // Add a character to the prefix
+        string newPrefix = prefix + alpha[i];
+        crack(alpha, newPrefix, arg, salt, n, k - 1);
+    }
+}
